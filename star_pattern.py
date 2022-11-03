bb2f466bf3f6b087376e88d6185b0d5e71eed65a
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2022-11-03 19:35:42 +0400

    Update star_pattern.py

--- star_pattern.py
+++ star_pattern.py
@@ -1,4 +1,12 @@
+#Method 1
 n = int(input("Enter the number of rows: "))
 
 for i in range(0,n+1):
 	print('*'*i)
+
+#Method 2
+n = int(input("Enter the number of rows: "))
+for i in range(n+1):
+	for j in range(i+1):
+		print('* ',end='')
+	print("\n")
